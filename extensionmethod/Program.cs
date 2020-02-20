dea7d3ba8efb4ed70267dd99826d9a1e73dc86be
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 15:06:56 +0530

    Initial commit

---
+++ extensionmethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extensionmethod
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string str = "zing zing amazing";
+            int i = str.wordcount();
+            Console.WriteLine(i);
+        }
+    }
+   
+
+}
